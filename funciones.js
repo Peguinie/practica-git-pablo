da75508e728afb9768b06ee98acfd2733c01e05f
Peguinie <pguinie3012@g.educaand.es>
2026-04-25 19:55:25 +0200

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,11 @@
+// Función que suma dos números
+
+function sumar(a, b) { //Aquí se declara la función con ambos parámetros a y b (los dos números)
+  return a + b; // Aquí se devuelve el argumento como la suma de ambos
+}
+
+// Función que multiplica dos números
+
+function multiplicar(a, b) { //Aquí se declara la función con ambos parámetros a y b (los dos números)
+  return a * b; // Aquí se devuelve el argumento como el producto de ambos
+}
